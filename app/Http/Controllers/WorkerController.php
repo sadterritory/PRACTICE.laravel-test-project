cd2ea63de514b368563f13d4c23908ad0f9de845
Ruslan <InfantryManStart@mail.ru>
2025-04-04 16:24:45 +0700

    create first model

--- app/Http/Controllers/WorkerController.php
+++ app/Http/Controllers/WorkerController.php
@@ -6,5 +6,12 @@ use Illuminate\Http\Request;
 
 class WorkerController extends Controller
 {
-    //
+    //action
+    public function index() {
+        return 'this is index action of WorkerController';
+    }
+
+    public function show() {
+        return 'this is show action of WorkerController';
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('workers', function () {
-    dd('hello');
-    return 'Its here workers';
-});
+Route::get('workers', [\App\Http\Controllers\WorkerController::class, 'index']);
+
+Route::get('workers/show', [\App\Http\Controllers\WorkerController::class, 'show']);
